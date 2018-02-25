1a08d4cc6180cf3117d9d0b23a57add74b275a12
Frogboy <fr0980y@gmail.com>
2018-02-24 21:28:15 -0500

    Initial Site
    First push.

---
+++ script.js
@@ -0,0 +1,99 @@
+function init () {
+	var data = [
+		{
+			name: "Race Core",
+			link: "https://github.com/FrogboyMV/RaceCore",
+			blurb: `
+				RPG Maker MV doesn’t have official support for races like other games such as
+				Dungeons & Dragons and Pathfinder RPG. Well, I’m here to fix that. Races have
+				been a part of RPGs since the beginning and a good solid core is needed to not
+				only create this feature but to extend its capabilities in the future.
+			`,
+			image: "img/races.jpg"
+		},
+		{
+			name: "Level-based Traits",
+			link: "https://github.com/FrogboyMV/LevelBasedTraits",
+			blurb: `
+				Anything you can do with Traits, you can do with these plugins except that now you
+				can have those traits kick in and/or expire based on the level of the actor.  This
+				includes things like elemental resistance, additional weapon and armor proficiency,
+				skills/spells, double gold, resist status ailments, poison attack etc.  The Race
+				version of this plugin extends the functionality of Race Core and allows you to grant
+				your races Traits based on their character's level.
+			`,
+			image: "img/party.png"
+		},
+		{
+			name: "Signature Skills",
+			link: "https://github.com/FrogboyMV/SignatureSkill",
+			blurb: `
+				Locke had Steal. Edgar had those cool Tools. Setzer had the very memorable random Slot
+				Machine thing. Signature Skills is where it's at. This plugin will give you that classic Final
+				Fantasy battle menu by allowing you to assign Signature Skills, replace Attack, Gaurd or
+				Items, add in extra commands and even adds in support for a TP-based Limit Break system
+				and a way for you to upgrade Signature Skills through equipment and items.
+			`,
+			image: "img/witch.jpg"
+		},
+		{
+			name: "Trigger Distance",
+			link: "https://github.com/FrogboyMV/TriggerDistance",
+			blurb: `
+				This plugin allows you to set up Player Touch activation that covers an area or line of
+				tiles around it. It is mainly used to avoid duplicating the same Event multiple times in
+				order to cover more than one square. By specifying your parameters in a comment on an Event
+				page, the Player Touch Trigger can be fired when your Player is at a specified distance from
+			 	the Event and/or on a certain axis. I've also added functionality to list specific modes of
+				travel that will trigger an event. Now you can fly your airship to a floating island.
+			`,
+			image: "img/airship.jpg"
+		},
+		{
+			name: "Blue Magic",
+			link: "https://github.com/FrogboyMV/BlueMagic",
+			blurb: `
+				This plugin enables a Blue Mage type ability where certain classes can learn
+				skills from the monsters they fight. Unlike the normal implementation of Blue
+				Magic, this version doesn't require that the learner be targeted with the skill
+				in question. All they have to do is be present when the skill is used in battle
+				and they will learn it. This allows your Blue Mage to learn passive skills like
+				heals and buffs.
+			`,
+			image: "img/blue_magic.jpg"
+		},
+		{
+			name: "Miscellaneous Plugins",
+			link: "https://github.com/FrogboyMV/MiscPlugins",
+			blurb: `
+				A series of small, miscellaneous plugins for RPG Maker MV. These plugins are typically
+				just installed and change a single aspect of a game.  Because they just make minor
+				changes and don't require any explanation on how to use them, they don't really need
+				to have their own spotlight and lumped into the miscellaneous section.
+			`,
+			image: "img/cat.png"
+		}
+	];
+
+	// Build Page
+	var main = document.getElementById("main");
+
+	for (var i in data) {
+		var d = data[i];
+		var html = `
+			<div>
+				<div style="background: url('${d.image}') no-repeat; background-size: cover;">
+
+				</div>
+				<div>
+					<a href="${d.link}">${d.name}</a>
+					<div class="blurb">
+						${d.blurb}
+					</div>
+				</div>
+			</div>
+		`;
+
+		main.innerHTML += html;
+	}
+};
